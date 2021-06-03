96151517915f6f50aec90be4748c78fc3fd7693f
quittung <quittung@users.noreply.github.com>
2021-06-04 01:13:57 +0200

    Implemented automatic generation of recurring departures as in #75

--- res/config/game_script/timetable_gui.lua
+++ res/config/game_script/timetable_gui.lua
@@ -616,6 +616,55 @@ function timetableGUI.makeArrDepWindow(lineID, stationID)
     if not menu.constraintTable then return end
     local conditions = timetable.getConditions(lineID,stationID, "ArrDep")
 
+    -- setup separation selector
+    local separationList = {1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30}
+    local separationCombo = api.gui.comp.ComboBox.new()
+    for k,v in ipairs(separationList) do 
+        separationCombo:addItem(v .. " min (" .. 60 / v .. "/h)")
+    end
+    separationCombo:setGravity(1,0)
+    
+    -- setup generate button
+    local generateButton = api.gui.comp.Button.new(api.gui.comp.TextView.new("Generate"), true)
+    generateButton:setGravity(1, 0)
+    generateButton:onClick(function()
+        -- preparation
+        if not conditions[1] then return end                        -- template condition not found
+        if separationCombo:getCurrentIndex() == -1 then return end  -- no separation selected
+        templateArrDep = conditions[1]
+
+        -- remove all conditions after the template
+        while conditions[2] do
+            timetable.removeCondition(lineID, stationID, "ArrDep", 2)
+        end
+
+        -- generate recurring conditions
+        local separation = separationList[separationCombo:getCurrentIndex()+1]
+        for i = 1, 60/separation-1 do
+            timetable.addCondition(lineID,stationID, {type = "ArrDep", ArrDep = {{
+                (templateArrDep[1] + i*separation) % 60,    -- arrival minute
+                templateArrDep[2],                          -- arrival second
+                (templateArrDep[3] + i*separation) % 60,    -- departure minute
+                templateArrDep[4]                           -- departure second
+            }}})
+        end
+
+        -- cleanup
+        timetableChanged = true
+        clearConstraintWindowLaterHACK = function()
+            timetableGUI.initStationTable()
+            timetableGUI.fillStationTable(UIState.currentlySelectedLineTableIndex, false)
+            timetableGUI.clearConstraintWindow()
+            timetableGUI.makeArrDepWindow(lineID, stationID)
+        end
+    end)
+
+    -- setup recurring departure generator
+    local recurringTable = api.gui.comp.Table.new(3, 'NONE')
+    recurringTable:addRow({api.gui.comp.TextView.new("Separation"),separationCombo,generateButton})
+    menu.constraintTable:addRow({recurringTable})
+
+
     -- setup add button
     local addButton = api.gui.comp.Button.new(api.gui.comp.TextView.new(UIStrings.add), true)
     addButton:setGravity(1,0)
